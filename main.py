24190f68eb4bd67b5a4af8b402e1d98e64abea9c
adityasriram05 <aditya.sriram54008@gmail.com>
2026-08-12 17:44:24 +0530

    adding functions folder and implementation

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 import time
+import functools
+
+import functions
 
 t = time.strftime("%d-%Y-%m- %H:%M:%S")
 print(t)
-
-
-def get_todos():
-    with open("todos.txt", "r") as file:
-        todos = file.readlines()
-    return todos
-
-def write_todos():
-    with open("todos.txt", "w") as file:
-        file.writelines(todos)
-
 
 while True:
     user_need = input("Enter add, show, edit, complete or exit: ")
@@ -22,24 +14,24 @@ while True:
     if user_need.startswith('add'):
 
         todo = user_need[4:] + '\n'
-        todos = get_todos()
+        todos = functions.get_todos()
         todos.append(todo)
-        write_todos()
+        functions.write_todos(todos)
 
     elif user_need.startswith('show'):
-        todos = get_todos()
+        todos = functions.get_todos()
         for index,todo in enumerate(todos):
             print(f"{index+1}: {todo}",end="")
 
     elif user_need.startswith('edit'):
         try:
-            todos = get_todos()
+            todos = functions.get_todos()
             if len(todos) < 1:
                 print("No todos added")
             number  = int(input("Enter the number of todo to edit: "))
             updated_todo = input("Enter the updated version: ")
             todos[number-1] = updated_todo + "\n"
-            write_todos()
+            functions.write_todos(todos)
 
         except IndexError:
             print(f"Invalid todo to edit because {number} does not exist")
@@ -47,7 +39,7 @@ while True:
 
     elif user_need.startswith('complete'):
         try:
-            todos = get_todos()
+            todos = functions.get_todos()
             if len(todos) < 1:
                 print("No todos added")
             for index,todo in enumerate(todos):
@@ -55,7 +47,7 @@ while True:
             number = int(input("Enter the number of todo to delete: "))
             deleted = todos.pop(number-1)
             print(f"{number}: {deleted.strip('\n')} has been deleted")
-            write_todos()
+            functions.write_todos(todos)
 
         except IndexError:
             print(f"The {number} numbered todo does not exist")
