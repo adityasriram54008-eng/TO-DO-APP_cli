1677ae32294355d911b86b183eee6e80f51c9510
adityasriram05 <aditya.sriram54008@gmail.com>
2026-08-12 22:20:38 +0530

    implemented edit

--- gui.py
+++ gui.py
@@ -3,27 +3,61 @@ import FreeSimpleGUI as sg
 
 label = sg.Text('Type in a TO-DO:')
 #inside the gui, there's a label, i mean like a header
+
 input_box = sg.InputText(tooltip= 'Enter TO-DO', key = 'TO-DO')
 #inside the label you can input using this, this is where you enter
+
 add_button = sg.Button('Add')
 # a clickable button
 
-window = sg.Window('My TO-DO App',layout= [[label] , [input_box,add_button]], font = ('Helvetica', 12))
+list_box = sg.Listbox(values=functions.get_todos(), key = 'todos', enable_events = True, size = [45,10])
+#a list box to be displayed in gui and has values from get_todos
+
+edit_button = sg.Button('Edit')
+
+window = sg.Window('My TO-DO App',layout= [[label] , [input_box,add_button], [list_box,edit_button]], font = ('Helvetica', 12))
 #the window of the gui i mean in the interface, with '..' as the title
 #in layout[] there's a nested list indicating the contents will be in one row
 
 while True:
     event , values = window.read()
-    # for eg window.read() for adding hi does == event = 'Add'  and values = {TO-DO:'hi'})
-    print(values)
-    print(event)
+    # for eg window.read() for adding hi using the 'Add' button does == event = 'Add'  and values = {TO-DO:'hi'})
+
     match event:
+    #checks for feature to be done similar to main.py
+
         case "Add":
             todos = functions.get_todos()
             new_todo = values['TO-DO'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            # todos gets the existing todos for get_todos() function if any or is just empty
+            # then new_todo gets assigned the todos entered, using the key value TO-DO which stores the entered input
+            #that new_todo is appeneded to the existing todos list with \n, and is written into todos.txt
+
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            #todos gets the value associated with the todos key from the listbox, i.e by clicking on a particular todos
+            #you get the event as todos with values, but by clicking on the edit button this case matches and the following operations are done
+
+            new_todo = values['TO-DO'] +'\n'
+
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+
+        case 'todos':
+            window['TO-DO'].update(value=values['todos'][0])
+
         case sg.WIN_CLOSED:
+        #case when close clicked, loop breaks out and the window is closed
+
             break
 
 window.close()
