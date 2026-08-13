2b71ef009446971b0a9d24696f34053d061d9f99
adityasriram05 <aditya.sriram54008@gmail.com>
2026-08-13 22:16:12 +0530

    implemented add new_todo

--- web.py
+++ web.py
@@ -3,10 +3,16 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state['new_todo']+'\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title('My Todos App')
 #creates a main title, could use st.subheader('') for sub headings and st.rite('') for normal text
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label = 'Enter a TODO', placeholder = 'Add new TODO...')
+st.text_input(label = 'Enter a TODO', placeholder = 'Add new TODO...', on_change = add_todo, key = 'new_todo')
